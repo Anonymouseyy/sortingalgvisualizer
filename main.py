07c5cdd208a4be6b4f693f4158bf043ab31675ac
Anonymouseyy <79242981+Anonymouseyy@users.noreply.github.com>
2022-05-29 14:52:34 -0700

    Added Insertion sort, need to make a better menu

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ largeFont = pg.font.Font("Roboto-Black.ttf", 40)
 current_arr = generate()
 current_rects = update_rects(current_arr)
 st = None
+sort_type = None
 sorting = False
 
 
@@ -60,11 +61,20 @@ while True:
         # Display sort button
         sort_button = pg.Rect(0, 0, width / 3, 80)
         sort_button.center = ((width-width/6-10), 50)
-        sort = mediumFont.render("Sort", True, black)
+        sort = mediumFont.render("Bubble Sort", True, black)
         sort_rect = sort.get_rect()
         sort_rect.center = sort_button.center
         pg.draw.rect(screen, white, sort_button)
         screen.blit(sort, sort_rect)
+
+        # Display sort button
+        isort_button = pg.Rect(0, 0, width / 3-50, 80)
+        isort_button.center = (width//2, 50)
+        isort = mediumFont.render("Insertion Sort", True, black)
+        isort_rect = isort.get_rect()
+        isort_rect.center = isort_button.center
+        pg.draw.rect(screen, white, isort_button)
+        screen.blit(isort, isort_rect)
 
         click, _, _ = pg.mouse.get_pressed()
         if click == 1:
@@ -77,33 +87,60 @@ while True:
                 time.sleep(0.2)
                 sorting = True
                 st = time.time()
+                sort_type = "Bubble"
+            elif isort_button.collidepoint(mouse):
+                time.sleep(0.2)
+                sorting = True
+                st = time.time()
+                sort_type = "Insertion"
 
     elif sorting:
         et = None
-        for i in range(len(current_arr)-1):
-            count = 0
-            for j in range(len(current_arr)-i-1):
-                if current_arr[j] > current_arr[j+1]:
-                    current_arr[j], current_arr[j+1] = current_arr[j+1], current_arr[j]
+        if sort_type == "Bubble":
+            for i in range(len(current_arr)-1):
+                count = 0
+                for j in range(len(current_arr)-i-1):
+                    if current_arr[j] > current_arr[j+1]:
+                        current_arr[j], current_arr[j+1] = current_arr[j+1], current_arr[j]
+                        screen.fill(black)
+                        current_rects = update_rects(current_arr)
+                        draw_arr(current_rects, [j, j+1])
+                        count += 1
+
+                    text = display_info("Bubble", st, time.time(), smallFont)
+                    screen.blit(text[0][0], text[0][1])
+                    screen.blit(text[1][0], text[1][1])
+                    pg.display.flip()
+
+                if not count:
+                    break
+
+        elif sort_type == "Insertion":
+            for i in range(1, len(current_arr)):
+                key = current_arr[i]
+
+                j = i-1
+                while j >= 0 and key < current_arr[j]:
+                    current_arr[j+1] = current_arr[j]
+                    j -= 1
+
                     screen.fill(black)
                     current_rects = update_rects(current_arr)
-                    draw_arr(current_rects, [j, j+1])
-                    count += 1
+                    draw_arr(current_rects, [j, i])
 
-                text = display_info("Bubble", st, time.time(), smallFont)
-                screen.blit(text[0][0], text[0][1])
-                screen.blit(text[1][0], text[1][1])
-                pg.display.flip()
+                    text = display_info("Insertion", st, time.time(), smallFont)
+                    screen.blit(text[0][0], text[0][1])
+                    screen.blit(text[1][0], text[1][1])
+                    pg.display.flip()
 
-            if not count:
-                break
+                current_arr[j+1] = key
 
         et = time.time()
+        text = display_info(sort_type, st, et, smallFont)
 
         for count, i in enumerate(current_arr):
             lst = [x for x in range(len(current_arr)) if x < i]
             screen.fill(black)
-            text = display_info("Bubble", st, et, smallFont)
             screen.blit(text[0][0], text[0][1])
             screen.blit(text[1][0], text[1][1])
             current_rects = update_rects(current_arr)
@@ -112,6 +149,7 @@ while True:
             time.sleep(0.01)
 
         sorting = False
+        sort_type = None
         st = None
 
     draw_arr(current_rects)
